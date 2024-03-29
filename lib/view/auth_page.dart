1b361dd801e4b45a22a0b1b3ae3fd7406c925a98
Hubaib19 <hubaib0019@gmail.com>
2024-03-29 14:06:05 +0530

    implemented firebase

---
+++ lib/view/auth_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthenticanScreen extends StatelessWidget {
+  const AuthenticanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
