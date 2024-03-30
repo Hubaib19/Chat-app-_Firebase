805b87335c2113207dd92463c31ccd0b4e8c30b9
Hubaib19 <hubaib0019@gmail.com>
2024-03-30 11:48:01 +0530

    working on authenticaction

---
+++ lib/view/signup_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+
+
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xFF122643),
+        body: Stack(
+          children: [
+            Positioned(
+              right: 1,
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                    color: Color(0xFF172F4D),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(370),
+                    )),
+              ),
+            ),
+            Positioned(
+                bottom: 1,
+                child: Container(
+                  width: 420,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF172F4D),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(230),
+                          topRight: Radius.circular(230))),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
